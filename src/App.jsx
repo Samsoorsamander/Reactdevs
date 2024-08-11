2ee0130462616c21733e16936d7814b9a91ff50b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-11 09:51:25 +0430

    inline event handling

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,13 @@ function App() {
 }
 
 export default function Button() {
-  function handleClick() {
-    alert("You clicked me");
-  }
-  return <button onClick={handleClick}>I dont do anyting</button>;
+  return (
+    <button
+      onClick={function handleClick() {
+        alert("this is inline event handling");
+      }}
+    >
+      Click me
+    </button>
+  );
 }
