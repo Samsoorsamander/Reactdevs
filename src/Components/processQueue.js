a8e85b15bfa1aaa5063f8fa633d8006b1ab8ffd9
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-13 10:16:11 +0430

    Challenge completed

--- src/Components/processQueue.js
+++ src/Components/processQueue.js
@@ -1,15 +1,5 @@
 export function getFinalState(baseState, queue) {
   let finalState = baseState;
-<<<<<<< HEAD
-  for (let update of queue) {
-    if (typeof update === "function") {
-      finalState = update(finalState);
-    } else {
-      finalState = update;
-    }
-  }
-=======
->>>>>>> 00bc3b6 (get the expected rersult)
 
   return finalState;
 }
