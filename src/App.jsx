0e91ffbb6ec6b11656102f477b1a5d30161a9ecd
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-13 09:13:55 +0430

    before updating

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,7 @@ function Counter() {
       <h1>{number}</h1>
       <button
         onClick={() => {
-          setNumber((n) => n + 1);
-          setNumber((n) => n + 1);
+          setNumber(number + 5);
           setNumber((n) => n + 1);
         }}
       >
