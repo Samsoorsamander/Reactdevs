996ee1b0b44f75bf4cbcaee7aea7bee9a6cae640
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-13 09:08:03 +0430

    Batching in states

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,26 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <TrafficLight />
+      <Counter />
     </>
   );
 }
 
-function TrafficLight() {
-  const [walk, setWalk] = useState(true);
-
-  function handleClick() {
-    setWalk(!walk);
-    alert(walk ? "Stop is next" : "Walk is next");
-  }
+function Counter() {
+  const [number, setNumber] = useState(0);
 
   return (
     <>
-      <button onClick={handleClick}>Change to {walk ? "Stop" : "Walk"}</button>
-      <h1
-        style={{
-          color: walk ? "darkgreen" : "darkred",
+      <h1>{number}</h1>
+      <button
+        onClick={() => {
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
+          setNumber((n) => n + 1);
         }}
       >
-        {walk ? "Walk" : "Stop"}
-      </h1>
+        +3
+      </button>
     </>
   );
 }
