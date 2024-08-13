f0e95e9ca93c7cde691930797687fa9e76663b96
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-13 09:27:54 +0430

    Challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,33 @@ import "./App.css";
 export default function App() {
   return (
     <>
-      <Counter />
+      <RequestTracker />
     </>
   );
 }
 
-function Counter() {
-  const [number, setNumber] = useState(0);
+function RequestTracker() {
+  const [pending, setPending] = useState(0);
+  const [completed, setCompleted] = useState(0);
+
+  async function handleClick() {
+    setPending((p) => p + 1);
+    await delay(3000);
+    setPending((p) => p - 1);
+    setCompleted((c) => c + 1);
+  }
 
   return (
     <>
-      <h1>{number}</h1>
-      <button
-        onClick={() => {
-          setNumber(number + 5);
-          setNumber((n) => n + 1);
-          setNumber(42);
-        }}
-      >
-        +3
-      </button>
+      <h3>Pending: {pending}</h3>
+      <h3>Completed: {completed}</h3>
+      <button onClick={handleClick}>Buy</button>
     </>
   );
+}
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
