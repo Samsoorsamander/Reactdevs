9a221ff623f179f65b5d1fc46be7730ea96a2509
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-13 15:18:32 +0430

    using one event handler insted of three

--- src/App.jsx
+++ src/App.jsx
@@ -20,31 +20,31 @@ function Form() {
     email: "bhepworth@sculpture.com",
   });
 
-  function handleFirstNameChange(e) {
-    setPerson({ ...person, firstName: e.target.value });
-  }
-
-  function handleLastNameChange(e) {
-    setPerson({ ...person, lastName: e.target.value });
-  }
-
-  function handleEmailChange(e) {
-    setPerson({ ...person, email: e.target.value });
+  function handleChange(e) {
+    setPerson({ ...person, [e.target.name]: e.target.value });
   }
 
   return (
     <>
       <label>
         First name:
-        <input value={person.firstName} onChange={handleFirstNameChange} />
+        <input
+          name="firstName"
+          value={person.firstName}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Last name:
-        <input value={person.lastName} onChange={handleLastNameChange} />
+        <input
+          name="lastName"
+          value={person.lastName}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Email:
-        <input value={person.email} onChange={handleEmailChange} />
+        <input name="email" value={person.email} onChange={handleChange} />
       </label>
       <p>
         {person.firstName} {person.lastName} ({person.email})
