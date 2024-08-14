e8023313ec15890d43ee6436da37289a601923c8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-14 09:56:46 +0430

    updating array using spread

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import Canvas from "./Components/Canvas";
+import { useState } from "react";
 export default function App() {
   return (
     <>
-      <Canvas />
+      <List />
+    </>
+  );
+}
+
+let nextId = 0;
+
+function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState([]);
+
+  return (
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button
+        onClick={() => {
+          setArtists([...artists, { id: nextId++, name: name }]);
+        }}
+      >
+        Add
+      </button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
     </>
   );
 }
