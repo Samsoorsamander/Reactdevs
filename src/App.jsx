510c58fbd53cb4fa6e220163caeaab4f9b9eb2cd
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-14 10:15:37 +0430

    handling shapes using map

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,68 @@ import { useState } from "react";
 export default function App() {
   return (
     <>
-      <List />
+      <ShapeEditor />
     </>
   );
 }
 
-let initialArtists = [
-  { id: 0, name: "Marta Colvin Andrade" },
-  { id: 1, name: "Lamidi Olonade Fakeye" },
-  { id: 2, name: "Louise Nevelson" },
+let initialShapes = [
+  { id: 0, type: "circle", x: 50, y: 100 },
+  { id: 1, type: "square", x: 150, y: 100 },
+  { id: 2, type: "circle", x: 250, y: 100 },
 ];
 
-function List() {
-  const [artists, setArtists] = useState(initialArtists);
+function ShapeEditor() {
+  const [shapes, setShapes] = useState(initialShapes);
 
+  function handleClick() {
+    const nextShapes = shapes.map((shape) => {
+      if (shape.type === "square") {
+        // No change
+        return shape;
+      } else {
+        // Return a new circle 50px below
+        return {
+          ...shape,
+          y: shape.y + 50,
+        };
+      }
+    });
+    // Re-render with the new array
+    setShapes(nextShapes);
+  }
+  function handleSqure() {
+    const squareShape = shapes.map((shape) => {
+      if (shape.type === "circle") {
+        return shape;
+      } else {
+        return {
+          ...shape,
+          x: shape.x + 50,
+          y: shape.y + 50,
+        };
+      }
+    });
+    setShapes(squareShape);
+  }
   return (
     <>
-      <h1>Inspiring sculptors:</h1>
-      <ul>
-        {artists.map((artist) => (
-          <li key={artist.id}>
-            {artist.name}{" "}
-            <button
-              onClick={() => {
-                setArtists(artists.filter((a) => a.id !== artist.id));
-              }}
-            >
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
+      <button onClick={handleClick}>Move circles down!</button>
+      <button onClick={handleSqure}>Move square left!</button>
+      {shapes.map((shape) => (
+        <div
+          key={shape.id}
+          style={{
+            background: "purple",
+            position: "absolute",
+            left: shape.x,
+            top: shape.y,
+            borderRadius: shape.type === "circle" ? "50%" : "",
+            width: 20,
+            height: 20,
+          }}
+        />
+      ))}
     </>
   );
 }
