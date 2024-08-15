a140e6e7a421b185a507174fe37112d27f79f304
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-15 07:25:02 +0430

    insertion into arrya

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,46 @@ import { useState } from "react";
 export default function App() {
   return (
     <>
-      <CounterList />
+      <List />
     </>
   );
 }
 
-let initialCounters = [0, 0, 0];
+let nextId = 3;
+const initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
-function CounterList() {
-  const [counters, setCounters] = useState(initialCounters);
+function List() {
+  const [name, setName] = useState("");
+  const [artists, setArtists] = useState(initialArtists);
 
-  function handleIncrementClick(index) {
-    const nextCounters = counters.map((c, i) => {
-      if (i === index) {
-        // Increment the clicked counter
-        return c + 1;
-      } else {
-        // The rest haven't changed
-        return c;
-      }
-    });
-    setCounters(nextCounters);
+  function handleClick() {
+    const insertAt = 1; // Could be any index
+    const nextArtists = [
+      // Items before the insertion point:
+      ...artists.slice(0, insertAt),
+      // New item:
+      { id: nextId++, name: name },
+      // Items after the insertion point:
+      ...artists.slice(insertAt),
+    ];
+    setArtists(nextArtists);
+    setName("");
   }
 
   return (
-    <ul>
-      {counters.map((counter, i) => (
-        <li key={i}>
-          {counter}
-          <button
-            onClick={() => {
-              handleIncrementClick(i);
-            }}
-          >
-            +1
-          </button>
-        </li>
-      ))}
-    </ul>
+    <>
+      <h1>Inspiring sculptors:</h1>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={handleClick}>Insert</button>
+      <ul>
+        {artists.map((artist) => (
+          <li key={artist.id}>{artist.name}</li>
+        ))}
+      </ul>
+    </>
   );
 }
