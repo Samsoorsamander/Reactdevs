4bbd376f58a19bb0347fc1f83e95fca73c26b440
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-17 08:12:46 +0430

    unnesessary use of state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import { useState } from "react";
 
-let nextId = 3;
-const initialTodos = [
-  { id: 0, title: "Buy milk", done: true },
-  { id: 1, title: "Eat tacos", done: false },
-  { id: 2, title: "Brew tea", done: false },
-];
-
 export default function App() {
   return (
     <>
@@ -16,62 +9,32 @@ export default function App() {
 }
 
 function Form() {
-  const [answer, setAnswer] = useState("");
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState("typing");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
 
-  if (status === "success") {
-    return <h1>That's right!</h1>;
+  function handleFirstNameChange(e) {
+    setFirstName(e.target.value);
+    setFullName(e.target.value + " " + lastName);
   }
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    setStatus("submitting");
-    try {
-      await submitForm(answer);
-      setStatus("success");
-    } catch (err) {
-      setStatus("typing");
-      setError(err);
-    }
-  }
-
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
+  function handleLastNameChange(e) {
+    setLastName(e.target.value);
+    setFullName(firstName + " " + e.target.value);
   }
 
   return (
     <>
-      <h2>City quiz</h2>
+      <h2>Let’s check you in</h2>
+      <label>
+        First name: <input value={firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name: <input value={lastName} onChange={handleLastNameChange} />
+      </label>
       <p>
-        In which city is there a billboard that turns air into drinkable water?
+        Your ticket will be issued to: <b>{fullName}</b>
       </p>
-      <form onSubmit={handleSubmit}>
-        <textarea
-          value={answer}
-          onChange={handleTextareaChange}
-          disabled={status === "submitting"}
-        />
-        <br />
-        <button disabled={answer.length === 0 || status === "submitting"}>
-          Submit
-        </button>
-        {error !== null && <p className="Error">{error.message}</p>}
-      </form>
     </>
   );
-}
-
-function submitForm(answer) {
-  // Pretend it's hitting the network.
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== "lima";
-      if (shouldError) {
-        reject(new Error("Good guess but a wrong answer. Try again!"));
-      } else {
-        resolve();
-      }
-    }, 1500);
-  });
 }
