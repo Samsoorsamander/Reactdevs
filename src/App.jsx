5f30344021f17a54d422e056c30a956402bbb352
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-19 09:57:01 +0430

    Avoiding redundancy

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,14 @@ export default function App() {
 function Form() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [fullName, setFullName] = useState("");
+  const fullName = firstName + " " + lastName;
 
   function handleFirstNameChange(e) {
     setFirstName(e.target.value);
-    setFullName(e.target.value + " " + lastName);
   }
 
   function handleLastNameChange(e) {
     setLastName(e.target.value);
-    setFullName(firstName + " " + e.target.value);
   }
 
   return (
