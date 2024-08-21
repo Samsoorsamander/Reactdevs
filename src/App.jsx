43fe24a2bd22fde7367f93ece392378f2f76b8bb
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 18:10:33 +0430

    Challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,66 @@
 import { useState } from "react";
+import AddItem from "./AddItem.js";
+import PackingList from "./PackingList.js";
 
 export default function App() {
   return (
     <>
-      <Clock color="pink" time="12:00" />
+      <TravelPlan />
     </>
   );
 }
 
-function Clock(props) {
-  return <h1 style={{ color: props.color }}>{props.time}</h1>;
+let nextId = 3;
+const initialItems = [
+  { id: 0, title: "Warm socks", packed: true },
+  { id: 1, title: "Travel journal", packed: false },
+  { id: 2, title: "Watercolors", packed: false },
+];
+
+function TravelPlan() {
+  const [items, setItems] = useState(initialItems);
+
+  const total = items.length;
+  const packed = items.filter((item) => item.packed).length;
+  function handleAddItem(title) {
+    setItems([
+      ...items,
+      {
+        id: nextId++,
+        title: title,
+        packed: false,
+      },
+    ]);
+  }
+
+  function handleChangeItem(nextItem) {
+    setItems(
+      items.map((item) => {
+        if (item.id === nextItem.id) {
+          return nextItem;
+        } else {
+          return item;
+        }
+      })
+    );
+  }
+
+  function handleDeleteItem(itemId) {
+    setItems(items.filter((item) => item.id !== itemId));
+  }
+
+  return (
+    <>
+      <AddItem onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onChangeItem={handleChangeItem}
+        onDeleteItem={handleDeleteItem}
+      />
+      <hr />
+      <b>
+        {packed} out of {total} packed!
+      </b>
+    </>
+  );
 }
