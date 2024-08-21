62f01db1cbee7ee9b1596e9d2d50f13ccf6305df
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 15:52:07 +0430

    another way of completing the challenge

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,11 @@ import { useState } from "react";
 export default function App() {
   return (
     <>
-      <Clock color="red" time="12:00" />
+      <Clock color="pink" time="12:00" />
     </>
   );
 }
 
 function Clock(props) {
-  const [color, setColor] = useState("red");
-  function handleColor() {
-    setColor("pink");
-  }
-  return (
-    <>
-      <h1 style={{ color: color }}>{props.time}</h1>
-      <button onClick={handleColor}>Change Color</button>
-    </>
-  );
+  return <h1 style={{ color: props.color }}>{props.time}</h1>;
 }
