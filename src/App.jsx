b15b54e1c4d265c1dff162aaa1f96b7cd8192a77
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 15:28:33 +0430

    flating nested states

--- src/App.jsx
+++ src/App.jsx
@@ -9,33 +9,71 @@ export default function App() {
   );
 }
 
-function PlaceTree({ place }) {
-  const childPlaces = place.childPlaces;
-  return (
-    <li>
-      {place.title}
-      {childPlaces.length > 0 && (
-        <ol>
-          {childPlaces.map((place) => (
-            <PlaceTree key={place.id} place={place} />
-          ))}
-        </ol>
-      )}
-    </li>
-  );
-}
-
 function TravelPlan() {
   const [plan, setPlan] = useState(initialTravelPlan);
-  const planets = plan.childPlaces;
+
+  function handleComplete(parentId, childId) {
+    const parent = plan[parentId];
+    // Create a new version of the parent place
+    // that doesn't include this child ID.
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter((id) => id !== childId),
+    };
+    // Update the root state object...
+    setPlan({
+      ...plan,
+      // ...so that it has the updated parent.
+      [parentId]: nextParent,
+    });
+  }
+
+  const root = plan[0];
+  const planetIds = root.childIds;
   return (
     <>
       <h2>Places to visit</h2>
       <ol>
-        {planets.map((place) => (
-          <PlaceTree key={place.id} place={place} />
+        {planetIds.map((id) => (
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
         ))}
       </ol>
     </>
+  );
+}
+
+function PlaceTree({ id, parentId, placesById, onComplete }) {
+  const place = placesById[id];
+  const childIds = place.childIds;
+  return (
+    <li>
+      {place.title}
+      <button
+        onClick={() => {
+          onComplete(parentId, id);
+        }}
+      >
+        Complete
+      </button>
+      {childIds.length > 0 && (
+        <ol>
+          {childIds.map((childId) => (
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
+          ))}
+        </ol>
+      )}
+    </li>
   );
 }
