e3fff2639f2931cd26764cb7f66ee3d3bded77f7
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-21 20:44:19 +0430

    Sharing component states

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,42 @@
 import { useState } from "react";
-import { initialLetters } from "./Components/Data.js";
-import Letter from "./Components/Letter.js";
 
 export default function App() {
   return (
     <>
-      <MailClient />
+      <Accordion />
     </>
   );
 }
 
-function MailClient() {
-  const [letters, setLetters] = useState(initialLetters);
-  const [highlightedId, setHighlightedId] = useState(null);
+function Panel({ title, children }) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <section className="panel">
+      <h3>{title}</h3>
+      {isActive ? (
+        <p>{children}</p>
+      ) : (
+        <button onClick={() => setIsActive(true)}>Show</button>
+      )}
+    </section>
+  );
+}
 
-  function handleHover(letterId) {
-    setHighlightedId(letterId);
-  }
-
-  function handleStar(starredId) {
-    setLetters(
-      letters.map((letter) => {
-        if (letter.id === starredId) {
-          return {
-            ...letter,
-            isStarred: !letter.isStarred,
-          };
-        } else {
-          return letter;
-        }
-      })
-    );
-  }
-
+function Accordion() {
   return (
     <>
-      <h2>Inbox</h2>
-      <ul>
-        {letters.map((letter) => (
-          <Letter
-            key={letter.id}
-            letter={letter}
-            isHighlighted={letter.id === highlightedId}
-            onHover={handleHover}
-            onToggleStar={handleStar}
-          />
-        ))}
-      </ul>
+      <h2>Almaty, Kazakhstan</h2>
+      <Panel title="About">
+        With a population of about 2 million, Almaty is Kazakhstan's largest
+        city. From 1929 to 1997, it was its capital city.
+      </Panel>
+      <Panel title="Etymology">
+        The name comes from <span lang="kk-KZ">алма</span>, the Kazakh word for
+        "apple" and is often translated as "full of apples". In fact, the region
+        surrounding Almaty is thought to be the ancestral home of the apple, and
+        the wild <i lang="la">Malus sieversii</i> is considered a likely
+        candidate for the ancestor of the modern domestic apple.
+      </Panel>
     </>
   );
 }
