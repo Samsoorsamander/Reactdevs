56d0f13dcf6345f64fdd8251cddcaa70c5ff9ba6
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-23 08:27:44 +0430

    challenge 1 completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,27 @@ import { useState } from "react";
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
+
+  return (
+    <div>
+      {showHint && (
         <p>
           <i>Hint: Your favorite city?</i>
         </p>
-        <Form />
-        <button
-          onClick={() => {
-            setShowHint(false);
-          }}
-        >
-          Hide hint
-        </button>
-      </div>
-    );
-  }
-  return (
-    <div>
+      )}
       <Form />
+
       <button
         onClick={() => {
-          setShowHint(true);
+          setShowHint(!showHint);
         }}
       >
-        Show hint
+        {showHint ? "Hide Hint" : "Show Hint"}
       </button>
     </div>
   );
 }
+
 function Form() {
   const [text, setText] = useState("");
   return <textarea value={text} onChange={(e) => setText(e.target.value)} />;
