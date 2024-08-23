5837ec7f47ca1a42db9315dc94cd48fa4de6a4d1
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-08-23 08:53:25 +0430

    Challenge 3: reset the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,10 @@
 import { useState } from "react";
+import ContactManager from "./Components/ContactManager";
 
 export default function App() {
-  const [reverse, setReverse] = useState(false);
-  let checkbox = (
-    <label>
-      <input
-        type="checkbox"
-        checked={reverse}
-        onChange={(e) => setReverse(e.target.checked)}
-      />
-      Reverse order
-    </label>
-  );
-  if (reverse) {
-    return (
-      <>
-        <Field key="last name" label="Last name" />
-        <Field key="first name" label="First name" />
-        {checkbox}
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Field key="first name" label="First name" />
-        <Field key="last name" label="Last name" />
-        {checkbox}
-      </>
-    );
-  }
-}
-
-function Field({ label }) {
-  const [text, setText] = useState("");
   return (
-    <label>
-      {label}:{" "}
-      <input
-        type="text"
-        value={text}
-        placeholder={label}
-        onChange={(e) => setText(e.target.value)}
-      />
-    </label>
+    <>
+      <ContactManager />
+    </>
   );
 }
